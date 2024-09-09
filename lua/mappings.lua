725d979a946d6b3b4debcfa919c19b54426a953d
chkg2a <xshalltearvamps@gmail.com>
2024-09-09 21:18:47 +0530

    initial commit

---
+++ lua/mappings.lua
@@ -0,0 +1,67 @@
+require "nvchad.mappings"
+
+
+-- add yours here
+local map = vim.keymap.set
+
+-- telescope
+local builtin = require "telescope.builtin"
+map("n", "<leader>ff", builtin.find_files, {})
+map("n", "<leader>fg", builtin.live_grep, {})
+map("n", "<leader>fg", builtin.live_grep, {})
+map("n", "<leader>fb", builtin.buffers, {})
+map("n", "<leader>fh", builtin.help_tags, {})
+map("n", "<leader>fo", builtin.oldfiles, {})
+map("n", "<leader>fz", builtin.current_buffer_fuzzy_find, {})
+map("n", "<leader>cm", builtin.git_commits, {})
+map("n", "<leader>gt", builtin.git_status, {})
+map("n", "<leader>ma", builtin.marks, {})
+map("n", "<leader>fa", function() 
+  builtin.find_files({ follow = true, no_ignore = true, hidden = true }) 
+end, {})
+
+-- ui
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader><leader>v", "<cmd>vsplit<cr>", { desc = "Virtical Split" })
+map("n", "<C-l>", "<C-w>l", { desc = "window left panel" })
+map("n", "<C-h>", "<C-w>h", { desc = "window right panel" })
+map("n", "<A-l>", "<C-w>l", { desc = "window left panel" })
+map("n", "<A-h>", "<C-w>h", { desc = "window right panel" })
+map("n", "<A-j>", "<C-w>j", { desc = "window up panel" })
+map("n", "<A-k>", "<C-w>k", { desc = "window down panel" })
+map("n", "<A-S-h>", "<cmd>2winc > <cr>", { desc = "increase window left panel" })
+map("n", "<A-S-l>", "<cmd>2winc < <cr>", { desc = "increase window right panel" })
+map("n", "<C-5>", "<cmd>vimgrep /\\w\\+/j % | copen<cr>", { desc = "quick fix" })
+map("n", "<leader>e", "<cmd> NvimTreeToggle <CR>", { desc = "Nvim Tree" })
+map("n", "<leader>r", "<cmd> RunCode <CR>", { desc = "Run Code" })
+map({ "n", "v" }, "<leader>]", ":Gen<CR>")
+
+-- Handle Terminals
+map({ "n", "t" }, "<C-8>", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "Terminal Toggle Floating term" })
+map({ "n", "t" }, "<C-9>", function()
+  require("nvchad.term").toggle { pos = "vsp", id = "vsplit", size = "0.3" }
+end, { desc = "Terminal Toggle Floating term" })
+map({ "n", "t" }, "<C-0>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "split" }
+end, { desc = "Terminal Toggle Floating term" })
+
+-- Yank Ring
+map({"n","x"}, "p", "<Plug>(YankyPutAfter)")
+map({"n","x"}, "P", "<Plug>(YankyPutBefore)")
+map({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
+map({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
+
+map("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+map("n", "<c-n>", "<Plug>(YankyNextEntry)")
+
+map("n","<leader>p", function()
+  require("telescope").extensions.yank_history.yank_history()
+end,{})
+
+-- Zen
+map("n", "<leader>z", "<cmd> ZenMode <CR>")
+
+-- URL
+map("n", "gx", "<cmd>URLOpenUnderCursor <CR>")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,334 @@
+return {
+  {
+    "gbprod/yanky.nvim",
+    opts = {},
+    config = function()
+      require("yanky").setup {
+        ring = {
+          history_length = 100,
+          storage = "shada",
+          storage_path = vim.fn.stdpath "data" .. "/databases/yanky.db", -- Only for sqlite storage
+          sync_with_numbered_registers = true,
+          cancel_event = "update",
+          ignore_registers = { "_" },
+          update_register_on_cycle = false,
+        },
+        picker = {
+          select = {
+            action = nil, -- nil to use default put action
+          },
+          telescope = {
+            use_default_mappings = true, -- if default mappings should be used
+            mappings = nil, -- nil to use default mappings or no mappings (see `use_default_mappings`)
+          },
+        },
+        system_clipboard = {
+          sync_with_ring = true,
+          clipboard_register = nil,
+        },
+        highlight = {
+          on_put = true,
+          on_yank = true,
+          timer = 500,
+        },
+        preserve_cursor_position = {
+          enabled = true,
+        },
+        textobj = {
+          enabled = true,
+        },
+      }
+    end,
+    lazy = false,
+  },
+  {
+    "folke/zen-mode.nvim",
+    lazy = false,
+  },
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup {
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {},
+          },
+        },
+      }
+      require("telescope").load_extension "ui-select"
+    end,
+    lazy = false,
+  },
+  {
+    "David-Kunz/gen.nvim",
+    config = function()
+      require("gen").setup {}
+    end,
+    lazy = false,
+  },
+  {
+    "monkoose/neocodeium",
+    event = "VeryLazy",
+    config = function()
+      local neocodeium = require "neocodeium"
+      neocodeium.setup()
+      vim.keymap.set("i", "<A-cr>", neocodeium.accept)
+    end,
+  },
+  {
+    "ahmedkhalf/project.nvim",
+    config = function()
+      require("project_nvim").setup()
+    end,
+    lazy = false,
+  },
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>1",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>2",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>3",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+    },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup()
+    end,
+  },
+  {
+    "f-person/git-blame.nvim",
+    lazy = false,
+  },
+  {
+    "folke/twilight.nvim",
+  },
+  {
+    "sontungexpt/url-open",
+    event = "VeryLazy",
+    cmd = "URLOpenUnderCursor",
+    config = function()
+      local status_ok, url_open = pcall(require, "url-open")
+      if not status_ok then
+        return
+      end
+      url_open.setup {}
+    end,
+  },
+  {
+    "nvimdev/lspsaga.nvim",
+    config = function()
+      require("lspsaga").setup {}
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
+  },
+  -- {
+  --   "windwp/nvim-autopairs",
+  --   event = "InsertEnter",
+  --   opts = {},
+  -- },
+  -- {
+  --   "folke/zen-mode.nvim",
+  --   cmd = { "ZenMode" },
+  -- },
+  {
+    "ggandor/leap.nvim",
+    dependencies = {
+      "tpope/vim-repeat",
+    },
+    config = function()
+      require("leap").add_default_mappings = false
+      vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+      vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+    end,
+    lazy = false,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "personal",
+          path = "~/.local/share/obsidian_ChK",
+        },
+      },
+
+      templates = {
+        subdir = "Templates",
+        date_format = "%Y-%m-%d-%a",
+        time_format = "%H:%M",
+      },
+      daily_notes = {
+        folder = "dailies",
+        date_format = "%Y-%m-%d",
+        alias_format = "%B %-d, %Y",
+        template = nil,
+      },
+      mappings = {
+        ["gf"] = {
+          action = function()
+            return require("obsidian").util.gf_passthrough()
+          end,
+          opts = { noremap = false, expr = true, buffer = true },
+        },
+        ["<leader>ch"] = {
+          action = function()
+            return require("obsidian").util.toggle_checkbox()
+          end,
+          opts = { buffer = true },
+        },
+      },
+      follow_url_func = function(url)
+        vim.fn.jobstart { "xdg-open", url } -- linux
+      end,
+      wiki_link_func = function(opts)
+        if opts.id == nil then
+          return string.format("[[%s]]", opts.label)
+        elseif opts.label ~= opts.id then
+          return string.format("[[%s|%s]]", opts.id, opts.label)
+        else
+          return string.format("[[%s]]", opts.id)
+        end
+      end,
+    },
+  },
+  {
+    "mbbill/undotree",
+    lazy = false,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "CRAG666/code_runner.nvim",
+    config = function()
+      require("code_runner").setup {
+        filetype = {
+          go = {
+            "cd $dir &&",
+            "go run $fileName",
+          },
+          reactjs = {
+            "cd $dir &&",
+            "npm run dev",
+          },
+          kotlin = {
+            "cd $dir &&",
+            "kotlinc $fileName &&",
+            "kotlin $fileNameWithoutExtKt",
+          },
+          cpp = {
+            "cd '$dir' &&",
+            "g++ $fileName -o out.$fileNameWithoutExt -Ofast -march=native -std=c++20 &&",
+            "/usr/bin/time -o memUsage.txt -f%M ./out.$fileNameWithoutExt &&",
+            "echo '' &&",
+            "cat memUsage.txt | awk '{printf \"Memory Usage: %.1f MB\\n\", $1/1024}' &&",
+            "rm ./out.$fileNameWithoutExt &&",
+            "rm ./memUsage.txt",
+          },
+          java = {
+            "cd $dir &&",
+            "javac $fileName &&",
+            "java $fileNameWithoutExt",
+          },
+          python = "python3 -u",
+          typescript = "deno run",
+          rust = {
+            "cd $dir &&",
+            "cargo build &&",
+            "cargo run",
+          },
+          js = {
+            "cd $dir &&",
+            "npm run dev",
+          },
+          cs = {
+            "cd $dir &&",
+            "mcs -out:$fileNameWithoutExt $fileName &&",
+            "$dir/$fileNameWithoutExt",
+          },
+        },
+      }
+    end,
+    lazy = false,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults
+        "vim",
+        "lua",
+        "markdown",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "json",
+        "c_sharp",
+        "sql",
+        "yaml",
+        "xml",
+        "tsx",
+      },
+      config = function()
+        require("nvim-treesitter.configs").setup {
+          ensure_installed = { "markdown", "markdown_inline" },
+          highlight = {
+            enable = true,
+          },
+        }
+      end,
+    },
+    lazy = false,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = false,
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+  },
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+}
